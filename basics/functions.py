63792a597d706c279fe1e29c6342a8a8d6aea927
Himanshu Kandpal <cse10324.sbit@gmail.com>
2019-10-07 17:02:31 +0530

    Create functions.py
    how function works

---
+++ basics/functions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 17:00:13 2019
+
+@author: Himanshu Kandpal
+"""
+
+def about_this_computer():
+  print("This computer is running on version Everest Puma")
+  print("This is your desktop")
+
+about_this_computer()
