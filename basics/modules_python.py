5817946e4d0f2ca21f6d299dad4fd78e93ce7c12
himanshu kandpa <cse10324.sbit@gmail.com>
2020-01-24 11:56:20 +0000

    overwrite import function

--- basics/modules_python.py
+++ basics/modules_python.py
@@ -1,1 +1,12 @@
 # module python code
+
+from math import sqrt
+
+print(sqrt(4))
+
+#defining it after the import statement will
+#overwrite the function with the same name
+def sqrt(param):
+    return param*100
+
+print(sqrt(4))
