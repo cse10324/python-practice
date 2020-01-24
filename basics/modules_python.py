b167749587e8cc7293f411612cd181fadfacec84
himanshu kandpa <cse10324.sbit@gmail.com>
2020-01-24 17:29:29 +0000

    Decimal operations cannot be performed with float

--- basics/modules_python.py
+++ basics/modules_python.py
@@ -11,12 +11,25 @@
 
 # print(sqrt(4))
 
-# Import Decimal below:
-from decimal import Decimal
+#Decimal datatype implmenetation
 
-# Fix the floating point math below:
-two_decimal_points = Decimal('0.2') + Decimal('0.69')
-print(two_decimal_points)
+# # Import Decimal below:
+# from decimal import Decimal
 
-four_decimal_points = Decimal('0.53') * Decimal('0.65')
-print(four_decimal_points)
+# # Fix the floating point math below:
+# two_decimal_points = Decimal('0.2') + Decimal('0.69')
+# print(two_decimal_points)
+
+# four_decimal_points = Decimal('0.53') * Decimal('0.65')
+# print(four_decimal_points)
+
+# With an integer.
+print(Decimal("10.5") + 3) # 13.5
+
+# With a float.
+# This will give an error
+# NameError: name 'Decimal' is not defined
+print(Decimal("10.5") + 3.0)
+
+# Instead, just use the Decimal object for float values.
+print(Decimal("10.5") + Decimal("3.0")) # 13.5
