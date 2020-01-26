9c119312bdeaaceb5c3dd60c8a6aed04091c43e8
himanshu kandpa <cse10324.sbit@gmail.com>
2020-01-26 05:47:40 +0000

    Check if a key is present or not without any error thrown

--- basics/dictionaries.py
+++ basics/dictionaries.py
@@ -26,4 +26,26 @@ plays = {songs:playcounts for songs, playcounts in zip(songs,playcounts)}
 
 plays.update({"Respect":{"album 1": "123 time played","album 2":"1231 time played", "album 3":"12311 time played"}})
 
-print(plays)
+#print(plays)
+
+#print the value associated with the key in dictionary
+# print(plays["Respect"])
+
+#try to access key which does not exist, should not throw error
+
+# zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+# zodiac_elements.update({"energy":"Not a Zodiac element"})
+
+# key_to_check = "Landmark 81"
+
+# if key_to_check in zodiac_elements:
+#   print(zodiac_elements["Landmark 81"])
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+key_to_check = "energy"
+
+#check if a key is present or not, will not throw error if not present
+if key_to_check in zodiac_elements:
+    print(zodiac_elements["energy"])
