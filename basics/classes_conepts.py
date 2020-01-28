8ab94fb8aa3bfa8c5bafab8e627d533fb92550e2
himanshu kandpa <cse10324.sbit@gmail.com>
2020-01-28 14:59:44 +0000

    Pass a parater to contructor and print its value on call

--- basics/classes_conepts.py
+++ basics/classes_conepts.py
@@ -41,3 +41,27 @@ converter = DistanceConverter()
 kms_in_5_miles = converter.how_many_kms(5)
 # print(kms_in_5_miles)
 
+# CONSTRUCTORS IN PYTHON
+# class Shouter:
+#     def __init__(self):
+#         print("HELLO!!!!")
+
+#__init__ method is called every time class is instantiated, is also called as constructor
+# one_call = Shouter()
+# second_call = Shouter()
+
+# pass the value to the constructor
+class Shouter:
+    def __init__(self, phrase):
+        #make sure the phrase is a string
+        if type(phrase) == str:
+            #shout it
+            print(phrase.upper())
+        else:
+            print("You sir has just entered a value other than a string")
+
+one_call = Shouter("shout")
+second_call = Shouter("let's hear it out")
+#pass a integer as a parameter now
+third_call = Shouter(4)    #this will not be printed
+
