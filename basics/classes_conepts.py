6e94235f18ce4e808695cd3aa734a242aee87dc6
himanshu kandpa <cse10324.sbit@gmail.com>
2020-01-28 14:31:23 +0000

    First paramter of a class method needs to be self

--- basics/classes_conepts.py
+++ basics/classes_conepts.py
@@ -1,5 +1,5 @@
 # Call type() on the integer 5 and print the results.
-print(type(5))
+#print(type(5))
 
 my_dict={}
 #Print out the type() of my_dict.
@@ -22,5 +22,14 @@ class CoolClass:
 
 #here we are creating an instance of class, obly declaring class is of no use, we have to make use of it by creating an instance of that class
 cool_instance = CoolClass()
+
+class Dog():
+    dog_time_dilation = 7 
+#methods inside class needs compulsory one paramter, the first argument in a method is always the object that is calling the method. Convention recommends that we name this first argument self.    
+    def time_explanation(self):
+        print("Dog experiences {} years for every 1 human year".format(self.dog_time_dilation))
+
+# pipi_pitbull = Dog()
+# pipi_pitbull.time_explanation()
 
 
