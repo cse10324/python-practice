050f9adc22ca722cd984fe67900d071e375946ce
himanshu kandpa <cse10324.sbit@gmail.com>
2020-02-09 14:59:57 +0000

    negative number

--- basics/basic.py
+++ basics/basic.py
@@ -6,4 +6,13 @@
 
 #join strings on a character
 joinedStrings = "-".join(["Codecademy","is","awesome"])
-print(joinedStrings)
+#print(joinedStrings)
+
+numbers = [0,254,2,-1,3]
+
+for number in numbers:
+    if number < 0:
+        print("Negative number alert!")
+        break
+    else:
+        print(number)
