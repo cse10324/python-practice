d7ef1299fe96c8c326271b91657ae88ed978485c
himanshu kandpa <cse10324.sbit@gmail.com>
2020-02-10 05:00:03 +0000

    Node challenge

--- basics/nodes.py
+++ basics/nodes.py
@@ -13,3 +13,18 @@ class Node:
     def set_link_node(self, link_node):
         self.link_node = link_node
     
+#Outside of Node, instantiate three nodes.
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+#yacko can keep track of dot and dot can keep up with wacko. wacko can’t keep track of anything though.
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+
+print(dots_data)
+print(wackos_data)
+
