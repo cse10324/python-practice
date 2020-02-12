bf559fc9f1b6f96ff6e3f58e92fbcca74a7ec308
himanshu kandpa <cse10324.sbit@gmail.com>
2020-02-12 10:46:49 +0000

    make sure the stack is not empty before pop is performed

--- basics/data structure/stack.py
+++ basics/data structure/stack.py
@@ -4,5 +4,17 @@ class Stack:
     def __init__(self):
         self.top_item = None
     
+    #Define your push method here
+    def push(self, value):
+        item = Node(value)
+        item.set_next_node(self.top_item)
+        self.top_item = item
+    
+    #Define your pop method
+    def pop(self):
+        item_to_remove = self.top_item
+        self.top_item = item_to_remove.get_next_node()
+        return item_to_remove.get_value()
+    
     def peek(self):
         return self.top_item.get_value()
