cf56f07e4575df237ea748e9c1759a6c73cca6a6
himanshu kandpa <cse10324.sbit@gmail.com>
2020-02-19 10:14:31 +0000

    dequeue method for the queue DS added

--- basics/data structure/QueueChallenge.py
+++ basics/data structure/QueueChallenge.py
@@ -6,4 +6,31 @@ class Queue:
         self.tail = None
         self.max_size = max_size
         self.size = 0
-        
+    
+    #inserting element into the queue
+    def enqueue(self, value):
+        if self.has_space():
+            item_to_add = Node(value)
+            print("Adding "+str(item_to_add.get_value())+ " to the queue!")
+            if self.is_empty():
+                self.head = item_to_add
+                self.tail = item_to_add
+            else:
+                self.tail.set_next_node(item_to_add)
+                self.tail = item_to_add
+            self.size += 1
+        else:
+            print("Sorry, no more room!")
+    
+    def peek(self):
+        if self.size > 0:
+            return self.head.get_value()
+        else:
+            print("No orders waiting!")
+    
+    def get_size(self):
+        return self.size
+    
+    def has_space(self):
+        return self.size == 0
+    
